2d7e7bfc86b2e441579c29a5b55258284792b0c6
Foxder15 <thanhduynt2604@gmail.com>
2023-12-20 21:38:51 +0700

    Check

--- src/main/java/com/code/FinalApplication.java
+++ src/main/java/com/code/FinalApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FinalApplication.class, args);
 	}
